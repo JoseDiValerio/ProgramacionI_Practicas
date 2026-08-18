9c6cf569ede01a1d5f2e97f1d6863d7932f61e33
JoseDiValerio <divalerio.j@gmail.com>
2026-08-18 00:53:02 -0400

    Update proyecto final

--- ProyectoFinal_Entrega/ControlReservasAulas/ControlReservasAulas/Program.cs
+++ ProyectoFinal_Entrega/ControlReservasAulas/ControlReservasAulas/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ControlReservasAulas;
+
+using (AppDbContext db = new AppDbContext())
+{
+    if (db.Database.CanConnect())
+    {
+        Console.WriteLine("Conexión correcta con SQL Server.");
+    }
+    else
+    {
+        Console.WriteLine("No se pudo conectar con SQL Server.");
+    }
+}
